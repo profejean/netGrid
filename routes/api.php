50849724ffe9660aba238fadc6f136252692ee95
profejean <profesorjeandiaz@gmail.com>
2022-10-20 19:10:11 -0400

    first commit favorite routes

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\FavoriteController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +17,18 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::middleware(['auth:sanctum'])->group(function () {
+
+    Route::get('favorite_index', [FavoriteController::class,'index'])->name('favorite_index');
+
+    Route::get('favorite_edit/{id}', [FavoriteController::class,'edit'])->name('favorite_edit');
+
+    Route::post('favorite_store', [FavoriteController::class,'store'])->name('favorite_store');
+
+    Route::patch('favorite_update/{id}', [FavoriteController::class,'update'])->name('favorite_update');
+
+    Route::delete('favorite_delete/{id}', [FavoriteController::class,'delete'])->name('favorite_delete');
+
 });
